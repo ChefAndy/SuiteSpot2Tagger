c3395e517a4e9c092ed01496a0a181beb75526e7
Andrew Silva <asilva@law.harvard.edu>
2015-02-17 16:54:22 -0500

    updated export

--- export.php
+++ export.php
@@ -5,13 +5,25 @@ $pageID=null;
 $m = new MongoClient();
 $db = $m->amesdb;
 
+$scandataMongoCollection = $db->scandata;
+$scanpagesMongoCollection = $db->scanpages;
+$tagsMongoCollection = $db->tags;
 
+$scandataIterator= $scandataMongoCollection->find();
 
-$collection = $db->tags;
-$tags=array();
-    $results= $collection->find();
-    foreach ($results as $result){
-    	print_r($result);
-    }
+foreach ($scandataIterator as $scandataresult){
+        $docID=strval($scandataresult['_id']);
+        echo $docID."\n";
 
-    
+        $scanpagesIterator= $scanpagesMongoCollection->find(array( 'objid' => ($docID) ));
+        foreach ($scanpagesIterator as $pageresult){
+                $pageID=strval($pageresult['_id']);
+                echo "\t".$pageID."\n";
+
+                $tagsIterator= $tagsMongoCollection->find(array( 'pageid' => ($pageID) ));
+                foreach ($tagsIterator as $tagsresult){
+                        $tagID=strval($tagsresult['_id']);
+                        echo "\t\t".$tagID." ".$tagsresult['tag']."\n";
+                }
+        }
+}
