aa5c9e3b74df64e525f2e30242fad093c09663a8
Andrew Silva <asilva@law.harvard.edu>
2015-02-24 00:57:42 -0500

    slightly cleaned up export. Create statements need to be corrected

--- export.php
+++ export.php
@@ -14,22 +14,22 @@ $mysql_ObjID=0;
 $mysql_PagID=0;
 $mysql_TagID=0;
 
-echo "CREATE TABLE objects (id INT, urn VARCHAR(50), title VARCHAR(256), label VARCHAR(256), imgct INT, thumbnail VARCHAR(50), drsID INT, mongoID VARCHAR(24))";
-echo "CREATE TABLE tags (id, , page_id, object_id, type, tag, mongoObjid, mognoPageID, mongoTagID)";
-echo "CREATE TABLE pages (id, object_id, sectionLabel, sectionPagestart, sectionPageend, sectionSeqstart, sectionSeqend, sectionLlabelrange, sectionSeqrange, sectionLink, label, pagelabel, sequence, pagenum, thumb, image, link, mongoObjid, mognoPageID)";
+echo "CREATE TABLE objects (id INT, urn VARCHAR(50), title VARCHAR(256), label VARCHAR(256), imgct INT, thumbnail VARCHAR(50), drsID INT, mongoID VARCHAR(24));";
+echo "CREATE TABLE tags (id, page_id, object_id, type, tag, mongoObjid, mognoPageID, mongoTagID);";
+echo "CREATE TABLE pages (id, object_id, sectionLabel, sectionPagestart, sectionPageend, sectionSeqstart, sectionSeqend, sectionLlabelrange, sectionSeqrange, sectionLink, label, pagelabel, sequence, pagenum, thumb, image, link, mongoObjid, mognoPageID);";
 
 foreach ($scandataIterator as $scandataresult){
 		$mysql_ObjID++;
         $docID=strval($scandataresult['_id']);
         echo "INSERT INTO objects (id, urn, title, label, imgct, thumbnail, drsID, mongoID) VALUES("
-        	.$mysql_ObjID.", "
+        	.$mysql_ObjID.", '"
         	.mysql_escape_string( strval($scandataresult['urn']))."', '"
-        	.mysql_escape_string(strval($scandataresult['title']))."', ''"
+        	.mysql_escape_string(strval($scandataresult['title']))."', '"
         	.mysql_escape_string(strval($scandataresult['label']))."', '"
         	.mysql_escape_string(strval($scandataresult['imgct']))."', '"
         	.mysql_escape_string(strval($scandataresult['thumbnail']))."', '"
         	.mysql_escape_string(strval($scandataresult['drsID']))."', '"
-        	.$docID.");";
+        	.$docID."');";
 
         $scanpagesIterator= $scanpagesMongoCollection->find(array( 'objid' => ($docID) ));
         foreach ($scanpagesIterator as $pageresult){
@@ -56,9 +56,9 @@ foreach ($scandataIterator as $scandataresult){
 
         	    echo "\tINSERT INTO pages (id, object_id, sectionLabel, sectionPagestart, sectionPageend, sectionSeqstart, sectionSeqend, sectionLlabelrange, sectionSeqrange, sectionLink, label, pagelabel, sequence, pagenum, thumb, image, link, mongoObjid, mognoPageID) VALUES("
 		        	.$mysql_PagID.", "
-		        	.$mysql_ObjID.", "
+		        	.$mysql_ObjID.", '"
 		        	.$sectionLabel."', '"
-		        	.$sectionPagestart."', ''"
+		        	.$sectionPagestart."', '"
 		        	.$sectionPageend."', '"
 		        	.$sectionSeqstart."', '"
 		        	.$sectionSeqend."', '"
@@ -74,7 +74,7 @@ foreach ($scandataIterator as $scandataresult){
 		        	.mysql_escape_string(strval($pageresult['link']))."', '"
 		        	.mysql_escape_string(strval($pageresult['objid']))."', '"
 		        	.$pageID."', '"
-		        .$docID.");";
+		        .$docID."');";
 
                 $tagsIterator= $tagsMongoCollection->find(array( 'pageid' => ($pageID) ));
                 foreach ($tagsIterator as $tagsresult){
@@ -84,12 +84,12 @@ foreach ($scandataIterator as $scandataresult){
 	        	    echo "\tINSERT INTO tags (id, , page_id, object_id, type, tag, mongoObjid, mognoPageID, mongoTagID) VALUES("
 			        	.$mysql_TagID.", "
 			        	.$mysql_PagID.", "
-			        	.$mysql_ObjID.", "
+			        	.$mysql_ObjID.", '"
 			        	.mysql_escape_string(strval($tagsresult['type']))."', '"
 			        	.mysql_escape_string(strval($tagsresult['tag']))."', '"
 			        	.$tagID."', '"
 			        	.$pageID."', '"
-			        .$docID.");";
+			        .$docID."');";
                 }
         }
 }
