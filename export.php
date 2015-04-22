c41b62ffdf04a2af41b8b214d130859476801435
Andrew Silva <asilva@law.harvard.edu>
2015-04-22 12:12:52 -0400

    added formatted mysql export and link

--- export.php
+++ export.php
@@ -14,23 +14,27 @@ $mysql_ObjID=0;
 $mysql_PagID=0;
 $mysql_TagID=0;
 
-echo "CREATE TABLE objects (id INT, urn VARCHAR(50), title VARCHAR(256), entrylabel VARCHAR(256), imgct INT, thumbnail VARCHAR(50), drsID INT, mongoID VARCHAR(24));";
-echo "CREATE TABLE tags (id INT, object_id INT, ttype VARCHAR(256), tag VARCHAR(256), mongoObjid VARCHAR(64), mognoPageID VARCHAR(64), mongoTagID VARCHAR(64));";
-echo "CREATE TABLE pages (id INT, object_id INT, sectionLabel VARCHAR(256), sectionPagestart  VARCHAR(256), sectionPageend  VARCHAR(256), sectionSeqstart INT, sectionSeqend INT, sectionLlabelrange VARCHAR(256), sectionSeqrange VARCHAR(256), sectionLink VARCHAR(256), entrylabel VARCHAR(256), pagelabel VARCHAR(256), sequence INT, pagenum VARCHAR(256), thumb VARCHAR(256), image VARCHAR(256), link VARCHAR(256), mongoObjid VARCHAR(64), mognoPageID VARCHAR(64));";
+header('Content-Type: application/octet-stream');
+header("Content-Transfer-Encoding: Binary"); 
+header("Content-disposition: attachment; filename=\"AmesOutput.sql\""); 
+
+echo "CREATE TABLE objects (id INT,\n urn VARCHAR(50),\n title VARCHAR(256),\n entrylabel VARCHAR(256),\n imgct INT,\n thumbnail VARCHAR(50),\n drsID INT,\n mongoID VARCHAR(24));\n\n";
+echo "CREATE TABLE tags (id INT,\n object_id INT,\n ttype VARCHAR(256),\n tag VARCHAR(256),\n mongoObjid VARCHAR(64),\n mognoPageID VARCHAR(64),\n mongoTagID VARCHAR(64));\n\n";
+echo "CREATE TABLE pages (id INT,\n object_id INT,\n sectionLabel VARCHAR(256),\n sectionPagestart  VARCHAR(256),\n sectionPageend  VARCHAR(256),\n sectionSeqstart INT,\n sectionSeqend INT,\n sectionLlabelrange VARCHAR(256),\n sectionSeqrange VARCHAR(256),\n sectionLink VARCHAR(256),\n entrylabel VARCHAR(256),\n pagelabel VARCHAR(256),\n sequence INT,\n pagenum VARCHAR(256),\n thumb VARCHAR(256),\n image VARCHAR(256),\n link VARCHAR(256),\n mongoObjid VARCHAR(64),\n mognoPageID VARCHAR(64));\n\n";
 
 
 foreach ($scandataIterator as $scandataresult){
 		$mysql_ObjID++;
         $docID=strval($scandataresult['_id']);
         echo "INSERT INTO objects (id, urn, title, entrylabel, imgct, thumbnail, drsID, mongoID) VALUES("
-        	.$mysql_ObjID.", '"
-        	.mysql_escape_string( strval($scandataresult['urn']))."', '"
-        	.mysql_escape_string(strval($scandataresult['title']))."', '"
-        	.mysql_escape_string(strval($scandataresult['entrylabel']))."', '"
-        	.mysql_escape_string(strval($scandataresult['imgct']))."', '"
-        	.mysql_escape_string(strval($scandataresult['thumbnail']))."', '"
-        	.mysql_escape_string(strval($scandataresult['drsID']))."', '"
-        	.$docID."');";
+                .$mysql_ObjID.",\n '"
+                .mysql_escape_string( strval($scandataresult['urn']))."',\n '"
+                .mysql_escape_string(strval($scandataresult['title']))."',\n '"
+                .mysql_escape_string(strval($scandataresult['entrylabel']))."',\n '"
+                .mysql_escape_string(strval($scandataresult['imgct']))."',\n '"
+                .mysql_escape_string(strval($scandataresult['thumbnail']))."',\n '"
+                .mysql_escape_string(strval($scandataresult['drsID']))."',\n '"
+        	.$docID."');\n";
 
         $scanpagesIterator= $scanpagesMongoCollection->find(array( 'objid' => ($docID) ));
         foreach ($scanpagesIterator as $pageresult){
@@ -56,41 +60,41 @@ foreach ($scandataIterator as $scandataresult){
                 if (array_key_exists('sectionLink', $pageresult)) { $sectionLink=mysql_escape_string( strval($pageresult['sectionLink'])); }
 
         	    echo "\tINSERT INTO pages (id, object_id, sectionLabel, sectionPagestart, sectionPageend, sectionSeqstart, sectionSeqend, sectionLlabelrange, sectionSeqrange, sectionLink, entrylabel, pagelabel, sequence, pagenum, thumb, image, link, mongoObjid, mognoPageID) VALUES("
-		        	.$mysql_PagID.", "
-		        	.$mysql_ObjID.", '"
-		        	.$sectionLabel."', '"
-		        	.$sectionPagestart."', '"
-		        	.$sectionPageend."', '"
-		        	.$sectionSeqstart."', '"
-		        	.$sectionSeqend."', '"
-		        	.$sectionLlabelrange."', '"
-		        	.$sectionSeqrange."', '"
-		        	.$sectionLink."', '"
-		        	.mysql_escape_string(strval($pageresult['entrylabel']))."', '"
-		        	.mysql_escape_string(strval($pageresult['pagelabel']))."', '"
-		        	.mysql_escape_string(strval($pageresult['sequence']))."', '"
-		        	.mysql_escape_string(strval($pageresult['pagenum']))."', '"
-		        	.mysql_escape_string(strval($pageresult['thumb']))."', '"
-		        	.mysql_escape_string(strval($pageresult['image']))."', '"
-		        	.mysql_escape_string(strval($pageresult['link']))."', '"
-		        	.mysql_escape_string(strval($pageresult['objid']))."', '"
-		        	.$pageID."', '"
-		        .$docID."');";
+                                .$mysql_PagID.",\n "
+                                .$mysql_ObjID.",\n '"
+                                .$sectionLabel."',\n '"
+                                .$sectionPagestart."',\n '"
+                                .$sectionPageend."',\n '"
+                                .$sectionSeqstart."',\n '"
+                                .$sectionSeqend."',\n '"
+                                .$sectionLlabelrange."',\n '"
+                                .$sectionSeqrange."',\n '"
+                                .$sectionLink."',\n '"
+                                .mysql_escape_string(strval($pageresult['entrylabel']))."',\n '"
+                                .mysql_escape_string(strval($pageresult['pagelabel']))."',\n '"
+                                .mysql_escape_string(strval($pageresult['sequence']))."',\n '"
+                                .mysql_escape_string(strval($pageresult['pagenum']))."',\n '"
+                                .mysql_escape_string(strval($pageresult['thumb']))."',\n '"
+                                .mysql_escape_string(strval($pageresult['image']))."',\n '"
+                                .mysql_escape_string(strval($pageresult['link']))."',\n '"
+                                .mysql_escape_string(strval($pageresult['objid']))."',\n '"
+                                .$pageID."',\n '"
+		        .$docID."');\n\n";
 
                 $tagsIterator= $tagsMongoCollection->find(array( 'pageid' => ($pageID) ));
                 foreach ($tagsIterator as $tagsresult){
 
 						$mysql_TagID++;
                         $tagID=strval($tagsresult['_id']);
-	        	    echo "\tINSERT INTO tags (id, page_id, object_id, ttype, tag, mongoObjid, mognoPageID, mongoTagID) VALUES("
-			        	.$mysql_TagID.", "
-			        	.$mysql_PagID.", "
-			        	.$mysql_ObjID.", '"
-			        	.mysql_escape_string(strval($tagsresult['type']))."', '"
-			        	.mysql_escape_string(strval($tagsresult['tag']))."', '"
-			        	.$tagID."', '"
-			        	.$pageID."', '"
-			        .$docID."');";
+                            echo "\tINSERT INTO tags (id,\n page_id,\n object_id,\n ttype,\n tag,\n mongoObjid,\n mognoPageID,\n mongoTagID) VALUES("
+                                        .$mysql_TagID.",\n "
+                                        .$mysql_PagID.",\n "
+                                        .$mysql_ObjID.",\n '"
+                                        .mysql_escape_string(strval($tagsresult['type']))."',\n '"
+                                        .mysql_escape_string(strval($tagsresult['tag']))."',\n '"
+                                        .$tagID."',\n '"
+                                        .$pageID."',\n '"
+                                .$docID."');\n\n";
                 }
         }
 }
